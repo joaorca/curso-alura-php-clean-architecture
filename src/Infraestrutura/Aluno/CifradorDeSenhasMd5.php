498fb7182f22dea80cfc30f9421bc4ffa2dbc446
João R C Almeida <joaorca@gmail.com>
2020-08-10 23:11:22 -0300

    Serviço de infraestrutura

---
+++ src/Infraestrutura/Aluno/CifradorDeSenhasMd5.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Arquitetura\Infraestrutura\Aluno;
+
+use Alura\Arquitetura\Dominio\Aluno\CifradorDeSenha;
+
+class CifradorDeSenhasMd5 implements CifradorDeSenha
+{
+
+    public function cifrar(string $senha): string
+    {
+        return md5($senha);
+    }
+
+    public function verificar(string $senhaEmTexto, string $senhaCifrada): bool
+    {
+        return md5($senhaEmTexto) === $senhaCifrada;
+    }
+}
---
+++ src/Infraestrutura/Aluno/CifradorDeSenhasPhp.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Arquitetura\Infraestrutura\Aluno;
+
+use Alura\Arquitetura\Dominio\Aluno\CifradorDeSenha;
+
+class CifradorDeSenhasPhp implements CifradorDeSenha
+{
+
+    public function cifrar(string $senha): string
+    {
+        return password_hash($senha, PASSWORD_ARGON2ID);
+    }
+
+    public function verificar(string $senhaEmTexto, string $senhaCifrada): bool
+    {
+        return password_verify($senhaEmTexto, $senhaCifrada);
+    }
+}
